626fc3a0aab2f1d088f0ba01918c01e077f9a222
Malcolm2324 <43822235+Malcolm2324@users.noreply.github.com>
2018-10-04 09:32:48 -0400

    exercise 3

--- Chapter 2 excersice.py
+++ Chapter 2 excersice.py
@@ -4,9 +4,13 @@ W3="and"
 W4="no"
 W5="play"
 W6="makes"
-W7="Jack"
+#W7="Jack"
 W8="a"
 W9="dull"
 W10="boy"
 
 print(W1,W2,W3,W4,W5,W6,W7,W8,W9,W10)
+#Traceback (most recent call last):
+  #File "C:\Users\cwris\OneDrive\Documents\GitHub\chapter-2-exercises-Malcolm2324\Chapter 2 excersice.py", line 12, in <module>
+    #print(W1,W2,W3,W4,W5,W6,W7,W8,W9,W10)
+#NameError: name 'W7' is not defined
